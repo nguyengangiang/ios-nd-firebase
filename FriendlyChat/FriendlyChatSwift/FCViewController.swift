ee31c53fd7b0808b8b00d6d8a90c812faff46372
Giang Nguyen <gn02.nguyen@gmail.com>
2021-04-25 12:26:12 -0700

    View larger image from photo message.

--- FriendlyChat/FriendlyChatSwift/FCViewController.swift
+++ FriendlyChat/FriendlyChatSwift/FCViewController.swift
@@ -274,7 +274,24 @@ extension FCViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-            // TODO: if message contains an image, then display the image
+        guard !messageTextField.isFirstResponder else {
+            return
+        }
+        let messageSnapshot: DataSnapshot! =  messages[(indexPath as NSIndexPath).row]
+        let message = messageSnapshot.value as! [String:String]
+        if let imageUrl = message[Constants.MessageFields.imageUrl] {
+            if let cachedImage = imageCache.object(forKey: imageUrl as NSString) {
+                showImageDisplay(cachedImage)
+            } else {
+                Storage.storage().reference(forURL: imageUrl).getData(maxSize: INT64_MAX) { (data, error) in
+                    guard error == nil else {
+                        print ("download error \(error!)")
+                        return
+                    }
+                    self.showImageDisplay(UIImage.init(data: data!)!)
+                }
+            }
+        }
     }
     
     // MARK: Show Image Display
